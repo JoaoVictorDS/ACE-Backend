f42b676892536cc54683da785ba31dff66d3a4a8
unknown <j_victor_jv97@hotmail.com>
2026-09-02 08:23:49 -0300

    fix: ajusta configurações de persistência e segurança do cookie

--- src/modules/auth/auth.constants.js
+++ src/modules/auth/auth.constants.js
@@ -1,7 +1,9 @@
 const COOKIE_OPTIONS = {
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
-    sameSite: 'None',
+    sameSite: process.env.NODE_ENV === 'production'
+        ? 'none'
+        : 'lax',
     maxAge: 7 * 24 * 60 * 60 * 1000 // 7 dias
 }
 
